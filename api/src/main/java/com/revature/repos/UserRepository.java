4ffeafde65e362437445077d0ae07904cdc2767a
Vinson <55514029+vinsonchin@users.noreply.github.com>
2021-07-22 14:42:46 -0600

    added the user classes back in

---
+++ api/src/main/java/com/revature/repos/UserRepository.java
@@ -0,0 +1,12 @@
+
+package com.revature.repos;
+
+import com.revature.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>
+{
+
+}
