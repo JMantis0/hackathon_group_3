be2361fade606b9b9c7de3d665a98573df5e6b64
SeanTaba <shahramtaba@gmail.com>
2021-07-22 09:54:06 -0600

    Initial commit

---
+++ src/main/java/com/revature/services/UserService.java
@@ -0,0 +1,21 @@
+package com.revature.services;
+
+import com.revature.entities.User;
+import com.revature.repos.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService
+{
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository)
+    {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user)
+    {
+        return userRepository.save(user);
+    }
+}
