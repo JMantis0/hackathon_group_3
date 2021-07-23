e7da546f5f1b7521f7a5efb577a7370951683ed5
SeanTaba <shahramtaba@gmail.com>
2021-07-22 22:32:12 -0600

    Characters are being loaded for a user - minor clean up

--- ui/client/src/App.tsx
+++ ui/client/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Login from "./components/Login";
 import {Provider, useSelector} from "react-redux";
-import {store} from "./store/store";
 import {authState} from "./states/auth-slice";
 import UserInfo from "./components/UserInfo";
 
@@ -13,9 +12,6 @@ function App() {
         <div>
             {!state.isLoggedIn ? <Login /> : <UserInfo/>}
             </div>
-        {/*    <div>*/}
-        {/*    {state.isLoggedIn && <UserInfo />}*/}
-        {/*</div>*/}
       </div>
   );
 }
